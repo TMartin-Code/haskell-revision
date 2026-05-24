5f32e5293234add62f5f625caf281b28885f2fbe
tommygm98 <tommygm98@gmail.com>
2026-05-24 16:55:20 +0100

    Week 7

--- week6.hs
+++ week6.hs
@@ -1,3 +1,58 @@
+-- Part 1
+hundredSquares :: Int
+hundredSquares = sum [ x*x | x <- [1..100]]
+
+grid :: Int -> Int -> [(Int,Int)]
+grid x y = [(a,b) | a <- [0..x], b <- [0..y]]
+
+square :: Int -> [(Int,Int)]
+square x = [(a,b) | a <- [0..(x-1)], b <- [0..(x-1)], a /= b] -- Adds squares to grid as long as first element isnt equal to the second
+
+find :: Eq a => a -> [(a, b)] -> [b]
+find i xs = [x | (k, x) <- xs, k == i] -- Retrieves each key value pair and only returns the value if they key and searched key match
+
+map' :: (a -> b) -> [a] -> [b]
+map' f xs = [f x | x <- xs]
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' f xs = [x | x <- xs, f x]
+
+
+replicate' :: Int -> a -> [a]
+replicate' count val = [val | _ <- [1.. count]]
+
+pyths :: Int -> [(Int, Int, Int)]
+pyths val = [(x, y, z) | x <- [1.. 10], y <- [1.. 10], z <- [1.. 10], (x*x) + (y*y) == (z*z)]
+
+
+lowers :: String -> Int
+lowers xs = length [x | x <- xs, x >= 'a' && x <= 'z']
+
+perfects :: Int -> [Int]
+perfects i = [x | x <- [1.. i], sum (factors x) == x]
+
+factors :: Int -> [Int]
+factors n = [x | x <- [1..n], n `mod` x == 0, x /= n]
+
+concatedGen' :: [(Int, Int)]
+concatedGen' = concat [[(x,y)| y <- [3,4]] | x <- [1,2]] -- Like Nested For Loop, x runs first, so x = 1 [(1,3),(1,4)] x = 2 [(2,3),(2,4)],
+                                                         -- Its then concated together to combine the 2 sublists
+
+positions :: Eq a => a -> [a] -> [Int]
+positions x xs = [i | (x',i) <- zip xs [0..], x == x'] -- Uses zip to find indexs of the matching values
+
+positions' :: Eq a => a -> [a] -> [Int]
+positions' x xs = find x (zip xs [0..])
+
+-- Part 2
+sumEvenSquares :: [Int] -> Int
+sumEvenSquares = foldl (\acc x -> if even x then acc + (x*x) else acc) 0
+
+takeWhilel :: (a -> Bool) -> [a] -> [a]
+--takeWhiler f xs = foldr (\x acc -> if f x then x : acc else []) [] xs
+takeWhilel f xs = foldl (\acc x -> if f x then acc ++ [x] else acc) [] xs
+
+
 main :: IO ()
 main = do
     putStrLn "Hello World!"
--- week7.hs
+++ week7.hs
@@ -1,3 +1,108 @@
+import System.IO
+import Data.List.Split (splitOn)
+import Data.Char (isDigit)
+
+-- Part 1
+requestName :: IO String
+requestName = do
+    putStrLn "Enter Your Name"
+    name <- getLine
+    pure name
+
+printName :: String -> IO ()
+printName x = do
+    putStrLn ("Hello " ++ x)
+
+convertMeasure :: IO ()
+convertMeasure = do
+    putStrLn "Enter Centimetres: "
+    input <- getLine
+    let cm = read input :: Double
+    let inch = cm / 2.54 :: Double
+    putStrLn (show cm ++ " Centimetres = " ++ show inch ++ " Inches")
+
+-- Part 2
+printMenu :: IO ()
+printMenu = do
+    putStrLn "1) Push a number on the stack \n2) Add the top two numbers on the stack. \n3) Print all the values on the stack. \n4) Exit"
+
+getMenuInput :: [Int] -> IO ()
+getMenuInput acc = do
+    printMenu
+    putStrLn "Select Option: "
+    input <- getLine
+    let choice = read input :: Int
+    handle choice acc
+
+    where
+        handle choice acc
+            | choice == 1 = do
+                putStrLn "Enter Num: "
+                input <- getLine
+                let val = read input :: Int
+                getMenuInput (val : acc)
+            | choice == 2 = do
+                if length acc < 2 then getMenuInput acc else do
+                    let a = head acc
+                    let b = head (tail acc)
+                    let restStack = drop 2 acc
+                    getMenuInput ((a+b) : restStack)
+            | choice == 3 = do
+                putStrLn ("Current Stack: " ++ show acc)
+                getMenuInput acc
+            | choice == 4 = putStrLn "Exiting calculator..."
+            | otherwise = do
+                putStrLn "Invalid Input"
+                getMenuInput acc
+
+-- Part 3
+data Movie = Movie {
+    name :: String,
+    year :: Int,
+    actors :: [String]
+    } deriving (Show)
+
+parseTitleAndYear :: String -> (String, Int)
+parseTitleAndYear rawTitle =
+    let titlePart = takeWhile (/= '(') rawTitle
+        yearPart  = filter isDigit rawTitle
+        parsedYear = if null yearPart then 0 else read yearPart :: Int
+    in (titlePart, parsedYear)
+
+processLine :: Handle -> [Movie] -> IO [Movie]
+processLine handle movies = do
+    isEnd <- hIsEOF handle
+    if isEnd
+        then return movies
+        else do
+            currLine <- hGetLine handle
+            let items = splitOn "/" currLine
+
+            if null items || head items == ""
+                then processLine handle movies
+                else do
+                    let (movieName, movieYear) = parseTitleAndYear (head items)
+                    let movieActors = tail items
+
+                    let newMovie = Movie {
+                        name   = movieName,
+                        year   = movieYear,
+                        actors = movieActors
+                    }                    }
+
+
+                    processLine handle (movies ++ [newMovie])
+
+readMovies :: String -> IO [Movie]
+readMovies fileName = do
+    handle <- openFile fileName ReadMode
+    moviesList <- processLine handle []
+    hClose handle
+    return moviesList
+
 main :: IO ()
 main = do
-    putStrLn "Hello World!"
+    -- Testing Part 2 by default
+    getMenuInput []
+    parsedMovies <- readMovies "movies.txt"
+    print parsedMovies
