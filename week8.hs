9975c624f9e1c728d5371fa654dd718e43cea704
tommygm98 <tommygm98@gmail.com>
2026-05-24 17:17:19 +0100

    Lab 8

--- week8.hs
+++ week8.hs
@@ -1,3 +1,20 @@
+-- Part 1
+-- Finished Last labs code
+
+-- Part 2
+risingSubseqLength :: [Int] -> Int
+risingSubseqLength [] = 0
+risingSubseqLength xs = go xs minBound 0 0
+  where
+    -- Arguments: remaining list, previous number, current chain length, max chain length seen so far
+    go [] _ currentMax overallMax = max currentMax overallMax
+    go (x:xs) prev currentMax overallMax
+        -- If it keeps increasing, grow the current chain
+        | x > prev  = go xs x (currentMax + 1) overallMax
+
+        -- If it drops, the chain snaps! Save our progress and reset current chain to 1 (starting with x)
+        | otherwise = go xs x 1 (max currentMax overallMax)
+
 main :: IO ()
 main = do
     putStrLn "Hello World!"
