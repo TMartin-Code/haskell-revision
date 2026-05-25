80187384a05efb2e21a5d4a73df2ec47ed25fed9
tommygm98 <tommygm98@gmail.com>
2026-05-25 15:00:36 +0100

    Added function definitions for applicative/functors/monad

--- types.hs
+++ types.hs
@@ -32,6 +32,9 @@
 -- Applies function to value inside a container, without affecting structure
 -- Uses fmap :: (a -> b) -> f a -> f b
 
+class Functor f where
+    fmap :: (a -> b) -> f a -> f b
+
 -- A normal function that expects a regular Int
 plusThree :: Int -> Int
 plusThree x = x + 3
@@ -56,6 +59,10 @@ functorExample3 = plusThree <$> Just 10    -- Result: Just 13
 -- implements <*> (Apply) extracts function from first box and value from second, applies them and places result in new box
 -- Also provides pure, takes normal value and wraps it in default box
 
+class Applicative f where
+    pure :: a -> f a
+    (<*>) :: f (a -> b) -> f a -> f b
+
 -- We have a function that takes two arguments
 add :: Int -> Int -> Int
 add x y = x + y
@@ -71,3 +78,34 @@ applicativeExample2 = add <$> Just 3 <*> Just 4       -- Result: Just 7
 -- If ANY of the boxes are Nothing, the whole thing safely collapses to Nothing
 applicativeExample3 :: Maybe Int
 applicativeExample3 = add <$> Just 3 <*> Nothing      -- Result: Nothing
+
+-------------------------------------------------------------------------------
+-- Monad:
+-------------------------------------------------------------------------------
+
+-- Each step of the computation decides what happens based off of the previous steps output
+-- Uses bind operator (>>=), takes a boxed value, unwraps and feeds it to a function that returns a box, returns resulting box without nesting it
+
+class Monad m where
+    (>>=) :: m a -> (a -> m b) -> m b
+    (>>=) :: m a -> m b -> m b
+    pure :: a -> m a
+
+-- A function that takes a normal Int but returns a Maybe box
+halveEven :: Int -> Maybe Int
+halveEven n
+    | even n    = Just (n `div` 2)
+    | otherwise = Nothing
+
+-- Using Monadic Bind (>>=) to chain operations
+monadExample1 :: Maybe Int
+monadExample1 = Just 20 >>= halveEven              -- Result: Just 10
+
+-- We can sequentially chain them. If any step returns Nothing,
+-- the chain short-circuits safely.
+monadExample2 :: Maybe Int
+monadExample2 = Just 20 >>= halveEven >>= halveEven -- Result: Just 5 (20 -> 10 -> 5)
+
+monadExample3 :: Maybe Int
+monadExample3 = Just 20 >>= halveEven >>= halveEven >>= halveEven
+-- Result: Nothing (because 5 is odd, the 3rd step returns Nothing)
